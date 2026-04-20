0149af3b2f8d533216d09b2f4edb78ebb59dc204
Digi-Blob <85460302+Digi-Blob@users.noreply.github.com>
2026-04-20 13:40:53 -0400

    Began pseudo code for api.js and game.js.

--- js/api.js
+++ js/api.js
@@ -1,1 +1,15 @@
 //the call backs for backend
+// Might be able to use a .JSON file in leu of a proper database.
+/*
+getScenName() {
+    //
+}
+
+getScenDesc() {
+    //
+}
+
+getSelectPoints(ID, selection) {
+    
+}
+*/
--- js/game.js
+++ js/game.js
@@ -1,1 +1,25 @@
 //game logic
+/*
+class LiveScenario {
+    constructor() {
+        this.scenID = "";
+        // this.selected = "";
+    }
+
+    setScenario(ID) {
+        this.scenID = ID;
+    }
+
+    calculateScore(selections) {
+        var pointSum = 0.0;
+        // temp1: the number of components that the user selected.    
+        for (var i = 0; i < temp1; i++) {
+            // getSelectPoints(ID, selection): returns the points associated with the selection based on the specified scenario.
+            // Will likely by a function implemented in "api.js".
+            pointSum += getPoints(this.scenID, selections[i]);
+        }
+    }
+}
+// The following should probably go into the HTML file.
+// var test = new LiveScenario();
+*/
